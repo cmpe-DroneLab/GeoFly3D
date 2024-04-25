b661be1dd7cbecd09f899c90f6fe25574fffa9b0
halilgurbuz <halil.gurbuz@hotmail.com>
2024-04-25 11:11:57 +0300

    Fixed some issues appearing on the preflight1 screen: - Unnecessary list refreshing / button disabling enabling - Disabling the buttons when the last task is removed Added comments to the code

--- UI/Preflight1/pre1.py
+++ UI/Preflight1/pre1.py
@@ -15,83 +15,86 @@ class Pre1(QWidget):
         self.ui = UI.Preflight1.pre1_design.Ui_Form()
         self.ui.setupUi(self)
 
+        self.map1 = None
         self.webView = QWebEngineView()
-        self.setup_map(41, 29, 12)
+        self.setup_map(39, 35, 5)
         self.ui.v_lay_right.addWidget(self.webView)
+
         self.ui.btn_delete_mission.clicked.connect(self.delete_mission)
-        self.ui.listWidget.itemClicked.connect(self.enable_buttons)
         self.ui.listWidget.itemSelectionChanged.connect(self.enable_buttons)
         self.refresh_mission_list()
 
+    # Creates a map given center point and zoom level
     def setup_map(self, lat, lon, zoom):
-        self.m = folium.Map(location=[lat, lon],
-                            zoom_start=zoom,
-                            control_scale=True,
-                            )
-
+        self.map1 = folium.Map(location=[lat, lon],
+                               zoom_start=zoom,
+                               control_scale=True, )
         self.save_map()
 
+    # Saves and shows the Map
     def save_map(self):
-        self.m.save('./UI/Preflight1/pre1map.html')
+        self.map1.save('./UI/Preflight1/pre1map.html')
         page = WebEnginePage(self.webView)
         self.webView.setPage(page)
         self.webView.setHtml(open('./UI/Preflight1/pre1map.html').read())
         self.webView.show()
 
+    # Adds marker to the Map
     def add_marker(self, lat, lon, popup_text):
-        folium.Marker([lat, lon], popup=popup_text).add_to(self.m)
+        folium.Marker([lat, lon], popup=popup_text).add_to(self.map1)
         self.save_map()
 
+    # Deletes the mission selected from the list from the database
     def delete_mission(self):
         selected_items = self.ui.listWidget.selectedItems()
+
+        # Find selected mission(s)
         for item in selected_items:
-            # Mission ID'sini al
             mission_id = int(item.text().split(":")[1].split(",")[0].strip())
-            # Veritabanından görevi bul
             mission = session.query(Mission).filter_by(mission_id=mission_id).first()
+
+            # Delete the mission from the database and refresh the Mission List
             if mission:
-                # Görevi veritabanından sil
                 session.delete(mission)
                 session.commit()
-                # Listeden görevi kaldır
-                # self.ui.listWidget.takeItem(self.ui.listWidget.row(item))
 
-                self.refresh_mission_list()
+        self.refresh_mission_list()
 
+    # Gets all missions from the database, adds them to the Mission List and adds markers to the Map
     def refresh_mission_list(self):
         self.ui.listWidget.clear()
 
         # Get all missions from the database
         missions = get_all_missions()
 
-        # Clear existing markers from the map
-        self.m = folium.Map(location=[39, 35], zoom_start=5, control_scale=True)
-
+        # Generate a new map in order to clear all markers
+        self.map1 = folium.Map(location=[39, 35], zoom_start=5, control_scale=True)
 
         for mission in missions:
             # Add missions to the Mission List
             item = QListWidgetItem(f"Mission ID: {mission.mission_id}, Status: {mission.mission_status}")
             self.ui.listWidget.addItem(item)
 
-            # Get mission center coordinates
+            # Get center points of the missions and put markers for them to the Map
             lat = mission.center_lat
             lon = mission.center_lon
-
-            # Add marker to the map
             if lat is not None:
                 popup_text = f"Mission ID: {mission.mission_id}, Status: {mission.mission_status}"
                 self.add_marker(lat, lon, popup_text)
 
-            # Save and display the updated map
-            self.save_map()
+        # Save and display the updated map
+        self.save_map()
 
-            # Disable buttons
-            self.disable_buttons()
+        # Disable Edit Mission and Delete Mission buttons
+        self.disable_buttons()
 
+    # Enables Edit Mission and Delete Mission buttons
     def enable_buttons(self):
-        self.ui.btn_edit_mission.setEnabled(True)
-        self.ui.btn_delete_mission.setEnabled(True)
+        if len(self.ui.listWidget.selectedIndexes()):
+            self.ui.btn_edit_mission.setEnabled(True)
+            self.ui.btn_delete_mission.setEnabled(True)
 
+    # Disables Edit Mission and Delete Mission buttons
     def disable_buttons(self):
         self.ui.btn_edit_mission.setEnabled(False)
         self.ui.btn_delete_mission.setEnabled(False)
