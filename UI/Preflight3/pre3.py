0d7d73dc90223a751a37ffeb46d2f9cfe502267f
halilgurbuz <halil.gurbuz@hotmail.com>
2024-04-25 21:57:00 +0300

    Improved pre3 design and functionality Added comments to the pre3 code

--- UI/Preflight3/pre3.py
+++ UI/Preflight3/pre3.py
@@ -1,64 +1,129 @@
+import json
+
 import folium
-from PyQt6.QtWidgets import QWidget
+from PyQt6.QtCore import QSize
+from PyQt6.QtWidgets import QWidget, QListWidgetItem
 from PyQt6.QtWebEngineWidgets import QWebEngineView
 
 from RoutePlanner import route_planner
+from UI.database import session, Mission, Drone
 from UI.web_engine_page import WebEnginePage
 import UI.Preflight3.pre3_design
+from UI.drone import Ui_Form
+from UI.Preflight2.pre2 import calculate_sw_ne_points, invert_coordinates
 
 
 class Pre3(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.m = None
         self.ui = UI.Preflight3.pre3_design.Ui_Form()
         self.ui.setupUi(self)
+
+        self.mission = None
+        self.mission_id = None
+        self.coords = None
+        self.coords_lon_lat = None
+        self.optimal_route = None
+        self.rotated_route = None
+        self.map3 = None
         self.webView = QWebEngineView()
         self.ui.v_lay_right.addWidget(self.webView)
 
+    # Loads mission information from database into relevant fields
+    def load_mission(self, mission_id):
 
-    def setup_map(self, coords, altitude):
-        self.coords = coords
-        self.altitude = altitude
+        if mission_id == 0:
+            exit(-1)
+        else:
+            self.mission = session.query(Mission).filter_by(mission_id=mission_id).first()
 
-        self.m = folium.Map(location=[35, 39],
-                            zoom_start=5,
-                            control_scale=True,
-                            )
+        self.mission_id = self.mission.mission_id
+        self.coords = json.loads(self.mission.coordinates)
+        self.coords_lon_lat = invert_coordinates(self.coords)
+
+        # Set mission id in the header box
+        self.ui.id_label.setText(str(self.mission_id))
+
+        # Set mission information box
+        self.ui.selected_area_value.setText(str(self.mission.selected_area))
+        self.ui.mission_time_value.setText(str(self.mission.estimated_mission_time))
+        self.ui.batt_required_value.setText(str(self.mission.required_battery_capacity))
+
+        # Load drones
+        self.refresh_drone_list()
+
+        # Set up the Map
+        self.setup_map()
+        self.calculate_route()
+        self.draw_optimal_route(self.optimal_route)
+        self.draw_rotated_route(self.rotated_route)
+
+    # Gets all matching drones from the database, adds them to the Drone List
+    def refresh_drone_list(self):
+        self.ui.listWidget.clear()
+
+        # Find all drones matching the Mission
+        drones = session.query(Drone).filter_by(mission_id=self.mission_id).all()
+        for drone in drones:
+            self.add_drone_to_list(drone)
+
+    # Adds given drone to the Drone List
+    def add_drone_to_list(self, drone):
+        new_drone_item = QListWidgetItem()
+        new_drone_item.setSizeHint(QSize(self.ui.listWidget.lineWidth(), 80))
+        new_drone_widget = QWidget()
+        new_drone_ui = Ui_Form()
+        new_drone_ui.setupUi(new_drone_widget)
+        new_drone_ui.id_text.setText(str(drone.drone_id))
+        new_drone_ui.model_text.setText(drone.model)
+        new_drone_ui.spare_batt_text.setText(str(drone.battery_no))
+        self.ui.listWidget.addItem(new_drone_item)
+        self.ui.listWidget.setItemWidget(new_drone_item, new_drone_widget)
+
+    # Creates a map
+    def setup_map(self):
+
+        self.map3 = folium.Map(location=[35, 39],
+                               zoom_start=5,
+                               control_scale=True, )
         self.save_map()
-        (optimal_route, rotated_route) = route_planner.plan_route(coords, altitude=altitude, intersection_ratio=0.8, angle_deg=20)
-        sw_point, ne_point = self.calculate_sw_ne_points(coords)
-        self.m.fit_bounds([sw_point, ne_point])
-        self.draw_optimal_route(optimal_route)
-        self.draw_rotated_route(rotated_route)
 
+        sw_point, ne_point = calculate_sw_ne_points(self.coords)
+        self.map3.fit_bounds([sw_point, ne_point])
+
+    # Saves and shows the map
     def save_map(self):
-        self.m.save('./UI/Preflight3/pre3map.html')
+        self.map3.save('./UI/Preflight3/pre3map.html')
         page = WebEnginePage(self.webView)
         self.webView.setPage(page)
         self.webView.setHtml(open('./UI/Preflight3/pre3map.html').read())
         self.webView.show()
 
-    def add_marker(self, lat, lon, popup_text):
-        folium.Marker([lat, lon], popup=popup_text).add_to(self.m)
-        self.save_map()
+    # Calculates route
+    def calculate_route(self):
+        (self.optimal_route, self.rotated_route) = route_planner.plan_route(coords=self.coords_lon_lat[:-1],
+                                                                            altitude=self.mission.altitude,
+                                                                            intersection_ratio=0.8,
+                                                                            angle_deg=20)
 
+    # Draws optimal route
     def draw_optimal_route(self, route):
-        
+
         # draw path nodes
         for point in route:
             folium.CircleMarker(point,
                                 radius=2,
                                 fill=True,
                                 fill_color="blue",
-                                fill_opacity=1).add_to(self.m)
+                                fill_opacity=1).add_to(self.map3)
 
         # draw path edges
         route_line = folium.PolyLine(locations=route, color='green', weight=2.5, opacity=0.8)
-        self.m.add_child(route_line)
+        self.map3.add_child(route_line)
         self.save_map()
 
+    # Draws rotated route
     def draw_rotated_route(self, route):
 
         # draw path nodes
@@ -67,28 +132,9 @@ class Pre3(QWidget):
                                 radius=2,
                                 fill=True,
                                 fill_color="orange",
-                                fill_opacity=1).add_to(self.m)
+                                fill_opacity=1).add_to(self.map3)
 
         # draw path edges
         route_line = folium.PolyLine(locations=route, color='orange', weight=2.5, opacity=0.8)
-        self.m.add_child(route_line)
+        self.map3.add_child(route_line)
         self.save_map()
-
-    def calculate_sw_ne_points(self, coords):
-        if not coords:
-            return None, None
-
-        lats = [coord[0] for coord in coords]
-        lons = [coord[1] for coord in coords]
-
-        min_lat = min(lats)
-        min_lon = min(lons)
-        max_lat = max(lats)
-        max_lon = max(lons)
-
-        sw_point = (min_lon, min_lat)  # Longitude comes first for southwest point
-        ne_point = (max_lon, max_lat)  # Longitude comes first for northeast point
-
-        return sw_point, ne_point
-
-    
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import sys
 
-from PyQt6.QtWidgets import QApplication, QMainWindow, QListWidgetItem
+from PyQt6.QtWidgets import QApplication, QMainWindow
 
 import UI.main_design
-from UI.database import Mission, session
 from UI.Preflight1.pre1 import Pre1
 from UI.Preflight2.pre2 import Pre2, invert_coordinates
 from UI.Preflight3.pre3 import Pre3
 from UI.Midflight.mid import Mid
 from UI.Postflight.post import Post
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -17,8 +17,8 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
 
         # Drop and Recreate all tables
-        #Base.metadata.drop_all(engine)
-        #Base.metadata.create_all(engine)
+        # Base.metadata.drop_all(engine)
+        # Base.metadata.create_all(engine)
 
         self.pre1 = Pre1()
         self.pre2 = Pre2()
@@ -43,56 +43,51 @@ class MainWindow(QMainWindow):
         self.pre3.ui.btn_return_back.clicked.connect(self.return_back_clicked)
         self.pre3.ui.btn_take_off.clicked.connect(self.take_off_clicked)
 
+    # PRE1 to PRE2
     def create_mission_clicked(self):
         mission_id = 0
         self.pre2.load_mission(mission_id)
         self.ui.stackedWidget.setCurrentIndex(1)
 
+    # PRE1 to PRE2
     def edit_mission_clicked(self):
         selected_item = self.pre1.ui.listWidget.selectedItems()[0]
         mission_id = int(selected_item.text().split(":")[1].split(",")[0].strip())
         self.pre2.load_mission(mission_id)
         self.ui.stackedWidget.setCurrentIndex(1)
 
+    # PRE2 to PRE1
     def cancel_mission_clicked(self):
         self.pre1.refresh_mission_list()
         self.ui.stackedWidget.setCurrentIndex(0)
 
+    # PRE2 to PRE1
     def save_mission_clicked(self):
         self.pre1.refresh_mission_list()
         self.ui.stackedWidget.setCurrentIndex(0)
 
+    # PRE3 to PRE2
     def return_back_clicked(self):
         self.ui.stackedWidget.setCurrentIndex(1)
 
+    # PRE2 to PRE3
     def start_mission_clicked(self):
-
         self.pre2.save_mission()
-        self.pre2.coords_lon_lat = invert_coordinates(self.pre2.coords)
-
-        # Copy mission information
-        self.pre3.ui.mission_time_value.setText(self.pre2.ui.mission_time_value.text())
-        self.pre3.ui.selected_area_value.setText(self.pre2.ui.selected_area_value.text())
-        self.pre3.ui.batt_required_value.setText(self.pre2.ui.batt_required_value.text())
         self.pre3.ui.batt_provided_value.setText(self.pre2.ui.batt_provided_value.text())
-
-        # Pass the route and altitude and create map
-        altitude_val = self.pre2.ui.slider_altitude.value()
-        route_coords = self.pre2.coords_lon_lat[:-1]
-        self.pre3.setup_map(route_coords, altitude_val)
-
+        self.pre3.load_mission(self.pre2.mission_id)
         self.ui.stackedWidget.setCurrentIndex(2)
 
+    # PRE3 to MID
     def take_off_clicked(self):
-
         vertices = self.pre3.coords
-        altitude = self.pre3.altitude
+        altitude = self.pre3.mission.altitude
 
         mission_thread = self.mid.take_off(vertices=vertices, flight_altitude=altitude)
         mission_thread.finished.connect(self.go_to_post)
 
         self.ui.stackedWidget.setCurrentIndex(3)
 
+    # MID to POST
     def go_to_post(self, msg, project_folder):
         print(msg)
         self.mid.threads.clear()
@@ -100,7 +95,8 @@ class MainWindow(QMainWindow):
         self.post.setup(project_folder)
 
         self.ui.stackedWidget.setCurrentIndex(4)
-        
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MainWindow()
