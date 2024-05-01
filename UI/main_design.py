6822f519de664d1c093beb31926798a0b01ed465
halilgurbuz <halil.gurbuz@hotmail.com>
2024-05-01 15:11:23 +0300

    - Improved UI

--- UI/main_design.py
+++ UI/main_design.py
@@ -34,7 +34,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("GeoFly3D", "GeoFly3D"))
 
 
 if __name__ == "__main__":
