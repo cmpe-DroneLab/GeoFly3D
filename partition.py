673e811d5258d2638c281c055bfb039bc751a344
Ali Nasra <52269552+AliNasra@users.noreply.github.com>
2024-05-09 20:27:11 +0300

    Update partition.py

--- partition.py
+++ partition.py
@@ -1,4 +1,4 @@
-from shapely.geometry import LineString,Polygon, Point
+from shapely.geometry import LineString,Polygon, Point,MultiPolygon
 import math
 import matplotlib.pyplot as plt
 
@@ -31,13 +31,13 @@ def orientation_decider(polygon,partition_line):
     
     # Determine if the line is to the right or left of the center of the polygon
     if angle_diff > math.pi:
-        return "Right"
+        return "right"
     elif angle_diff < -math.pi:
-        return "Left"
+        return "left"
     elif angle_diff > 0:
-        return "Right"
+        return "right"
     elif angle_diff < 0:
-        return "Left"
+        return "left"
     else:
         pass
 
@@ -69,11 +69,47 @@ def optimization_function(polygon_1,polygon_2):
     polygon_area_2 = polygon_1.area
     return abs(polygon_area_1-polygon_area_2)
 
-def partition_polygon(polygon,division_line):
-    # Split the shape along the line
-    divided_shapes = polygon.difference(division_line.buffer(0.000001))
-    print(divided_shapes)
-    return divided_shapes
+
+def plot_partition(polygon):
+    x, y = polygon.exterior.xy
+    # Plot the polygon
+    plt.plot(x, y, color='blue')  # Plot the exterior of the polygon in blue
+    plt.fill(x, y, color='lightblue', alpha=0.5)  # Fill the interior of the polygon in light blue with transparency
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title('Polygon Visualization')
+    plt.grid(True)
+    plt.axis('equal')
+    plt.show()
+
+
+def partition_polygon(polygon,line):
+    intersection_points = line.intersection(polygon.boundary)
+    
+    # Sort the intersection points along the line
+    sorted_intersection_points = sorted(intersection_points, key=lambda point: line.project(point))
+    
+    # If there are no intersection points, the line does not intersect the polygon
+    if not sorted_intersection_points:
+        return [polygon]
+    
+    # Create a list of coordinates representing the parts of the polygon divided by the line
+    divided_polygon_coords = []
+    for i in range(len(sorted_intersection_points) + 1):
+        if i == 0:
+            # Append the coordinates of the part of the polygon before the first intersection point
+            divided_polygon_coords.append([point for point in polygon.boundary.coords if Point(point) != sorted_intersection_points[i]])
+        elif i == len(sorted_intersection_points):
+            # Append the coordinates of the part of the polygon after the last intersection point
+            divided_polygon_coords.append([point for point in polygon.boundary.coords if Point(point) != sorted_intersection_points[i - 1]])
+        else:
+            # Append the coordinates of the part of the polygon between two consecutive intersection points
+            divided_polygon_coords.append([point for point in polygon.boundary.coords if Point(point) == sorted_intersection_points[i - 1] or Point(point) == sorted_intersection_points[i]])
+    
+    # Create polygons from the divided parts
+    divided_polygons = [Polygon(part) for part in divided_polygon_coords]
+    
+    return divided_polygons
 
 
 
@@ -84,38 +120,34 @@ def parallel_partitions(polygon, partition_line,start_vertex):
     interpartition_disp  = distance / (number_partition + 1)
     partition_direction  = orientation_decider(polygon,partition_line)
     partition_list       = []
+    rot_sign             = None
+    if partition_direction == "left":
+        rot_sign = 1
+    else:
+        rot_sign = -1
     for counter in range(number_partition):
         line_start, _  = partition_line.coords
         rotation_point = Point(line_start)
         for i in range(1,23):
-            angle_radians      = math.radians(i)  
+            angle_radians      = math.radians(i*rot_sign)  
             rotated_line       = rotate_line(partition_line,angle_radians)
             partitioned_shapes = partition_polygon(polygon,rotated_line)
-            print(partitioned_shapes)
+            if len(partitioned_shapes) == 1:
+                continue
             value              = optimization_function(partitioned_shapes[0],partitioned_shapes[1])
             partition_list.append([partitioned_shapes[0],partitioned_shapes[1],value])
         partition_line = partition_line.parallel_offset(interpartition_disp, side=partition_direction)
     return len(partition_list)
 
 
+def get_partitions(polygon_coords,line_start,line_end):
+    # Create a Polygon
+    polygon        = Polygon(polygon_coords)
+    partition_line = LineString([line_start, line_end])
+    part_len       = parallel_partitions(polygon,partition_line,Point(line_start))
+    print(part_len)
 
-polygon_coords = [(0, 0), (0, 3), (3, 3), (3, 0)]
-# Define the line that partitions the polygon
-partition_line = LineString([(0, 10), (0, -10)])
-# Create a Polygon
-polygon        = Polygon(polygon_coords)
-line_start, _  = partition_line.coords
-part_len       = parallel_partitions(polygon,partition_line,Point(line_start))
-print(part_len)
+
 # Extract the x and y coordinates of the polygon exterior
-x, y = polygon.exterior.xy
-
-# Plot the polygon
-plt.plot(x, y, color='blue')  # Plot the exterior of the polygon in blue
-plt.fill(x, y, color='lightblue', alpha=0.5)  # Fill the interior of the polygon in light blue with transparency
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.title('Polygon Visualization')
-plt.grid(True)
-plt.axis('equal')
-plt.show()
+polygon_coords = [(0, 0), (0, 3), (3, 3), (3, 0)]
+get_partitions(polygon_coords,(0,0),(0,3))
